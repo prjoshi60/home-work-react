de29d68b110fa789ad146f12b6de130766791a65
Prashant Joshi <prashant.joshi60@gmail.com>
2021-01-23 13:38:14 +0530

    Added the prettier in project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      The first working example. 
-    </div>
-  );
+  return <div className="App">The first working example.</div>;
 }
 
 export default App;
